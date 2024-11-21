81e881a25b1c711c90c254cdeb00c925510ecb66
thein3rovert <danielolaibi@gmail.com>
2024-11-21 06:09:56 +0000

    Basic Homepage

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h2>This is home page</h2>
+                <h5>Welcome to GeoInfo React application</h5>
+            </React.Fragment>
+        );
+    }
+}
+
+export default HomePage;
+
+
+
