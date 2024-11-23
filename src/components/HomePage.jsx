274f620b10fff9a2080f0d37334f5ef069279d9e
thein3rovert <danielolaibi@gmail.com>
2024-11-23 01:52:44 +0000

    Handle the selected country when click

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -12,7 +12,7 @@ const HomePage = () => {
       try {
         const response = await fetch("http://localhost:8080/api/countries/flag");
         const data = await response.json();
-        if(data.error === "false") {
+        if (data.error === "false") {
           setCountries(data.data);
         } else {
           console.error("Error fetching countries:", data.msg);
@@ -22,7 +22,7 @@ const HomePage = () => {
       }
     };
     fetchCountries();
-  },[]);
+  }, []);
 
 
   const handleInputChange = (event) => {
@@ -33,60 +33,66 @@ const HomePage = () => {
       country.name.toLowerCase().includes(value.toLowerCase())
     );
     setFilteredCountries(filtered);
-};
+  };
 
-    const handleSearch = (event) => {
-      event.preventDefault();
-      console.log("Searching for:", searchTerm);
-    };
+  const handleCountrySelection = (selectedCountryName) => {
+    setSearchTerm(selectedCountryName);
+    setFilteredCountries([]);
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    console.log("Searching for:", searchTerm);
+  };
 
 
-    return (
-      <div>
-        <div className="hero">
-          <h1>Welcome to GeoInfo</h1>
-          <p>A React application for exploring geographic information</p>
-          <button className="btn">Learn More</button>
-        </div>
-
-        {/* Search Bar */}
-        <div className="search-bar">
-          <form onSubmit={handleSearch}>
-            <input
-              type="text"
-              placeholder="Search for country..."
-              value={searchTerm}
-              onChange={handleInputChange}
-            />
-
-            <button type="submit">Search</button>
-          </form>
-          {searchTerm && (
-            <ul className="dropdown-list">
-              {filteredCountries.map(country => (
-                <li key={country.iso3}>
-                  <img
-                    src={country.flag}
-                    alt={country.name}
-                    style={{ width: '20px', marginRight: '10px' }}
-                  />
-                  {country.name}
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-
-        <section className="features">
-          <h2>Features</h2>
-          <ul>
-            <li>View geographic information on a map</li>
-            <li>Utilize line charts and bar charts for clear and insightful data representation.</li>
-            <li>Learn about different geographic locations, including population statistics and key information.</li>
-          </ul>
-        </section>
+  return (
+    <div>
+      <div className="hero">
+        <h1>Welcome to GeoInfo</h1>
+        <p>A React application for exploring geographic information</p>
+        <button className="btn">Learn More</button>
       </div>
-    );
+
+      {/* Search Bar */}
+      <div className="search-bar">
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            placeholder="Search for country..."
+            value={searchTerm}
+            onChange={handleInputChange}
+          />
+
+          <button type="submit">Search</button>
+        </form>
+        {searchTerm && (
+          <ul className="dropdown-list">
+            {filteredCountries.map(country => (
+              <li key={country.iso3}
+                onClick={() => handleCountrySelection(country.name)}>
+                <img
+                  src={country.flag}
+                  alt={country.name}
+                  style={{ width: '20px', marginRight: '10px' }}
+                />
+                {country.name}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+
+      <section className="features">
+        <h2>Features</h2>
+        <ul>
+          <li>View geographic information on a map</li>
+          <li>Utilize line charts and bar charts for clear and insightful data representation.</li>
+          <li>Learn about different geographic locations, including population statistics and key information.</li>
+        </ul>
+      </section>
+    </div>
+  );
 }
 
 export default HomePage;
