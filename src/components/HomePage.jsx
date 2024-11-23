99ccc824ed0a6d5e5cfa337b5524e4c120d10d0e
thein3rovert <danielolaibi@gmail.com>
2024-11-23 01:13:14 +0000

    Basic search Bar added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
-import './HomePage.css'
+import '../styles/HomePage.css'
 
 class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ''
+    };
+  }
+
+    handleInputChange = (event) => {
+      this.setState({ searchTerm: event.target.value});
+    };
+
+    handleSearch = (event) => {
+      event.preventDefault();
+      console.log( "Searching for:", this.state.searchTerm);
+    };
+
   render() {
     return (
       <div>
@@ -10,12 +26,26 @@ class HomePage extends Component {
           <p>A React application for exploring geographic information</p>
           <button className="btn">Learn More</button>
         </div>
+
+        {/* Search Bar */}
+        <div className="search-bar">
+          <form onSubmit={this.handleSearch}>
+            <input
+              type="text"
+              placeholder="Search for country"
+              value={this.state.searchTerm}
+              onChange={this.handleInputChange}
+            />
+            <button type="submit">Search</button>
+          </form>
+        </div>
+
         <section className="features">
           <h2>Features</h2>
           <ul>
             <li>View geographic information on a map</li>
-            <li>Explore line charts and bar charts for data visualization</li>
-            <li>Learn more about different geographic locations</li>
+            <li>Utilize line charts and bar charts for clear and insightful data representation.</li>
+            <li>Learn about different geographic locations, including population statistics and key information.</li>
           </ul>
         </section>
       </div>
@@ -24,3 +54,5 @@ class HomePage extends Component {
 }
 
 export default HomePage;
+
+// Todo: Review and commit the citypopulation and the simpleLineChart
